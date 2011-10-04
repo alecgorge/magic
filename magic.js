76b060da8c968a5759005a2167affc5e37385b9f
Alec Gorge <alecgorge@gmail.com>
2011-10-04 18:10:08 -0400

    first commit

---
+++ magic.js
@@ -0,0 +1,2 @@
+var inp = {"op":">","left":{"op":">","left":{"tag":"html"},"right":{"tag":"body"}},"right":{"op":"+","left":{"op":"+","left":{"tag":"header"},"right":{"op":">","left":{"tag":"div","id":"wrapper","class":["test","abc","xyz"],"props":{"key":"val"},"multi":"5"},"right":{"op":"+","left":{"tag":"article","class":["main"],"props":{}},"right":{"tag":"aside","class":["col"],"props":{}}}}},"right":{"tag":"footer"}}};
+
