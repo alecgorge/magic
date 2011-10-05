b6c5e2cf679fb422abb318e7af45426408a76aa6
Alec Gorge <alecgorge@gmail.com>
2011-10-04 23:10:33 -0400

    extra

--- magic.js
+++ magic.js
@@ -1,5 +1,3 @@
-var inp = {"op":">","left":{"op":">","left":{"tag":"html"},"right":{"tag":"body"}},"right":{"op":"+","left":{"op":"+","left":{"tag":"header"},"right":{"op":">","left":{"tag":"div","id":"wrapper","class":["test","abc","xyz"],"props":{"key":"val"},"multi":"5"},"right":{"op":"+","left":{"tag":"article","class":["main"],"props":{}},"right":{"tag":"aside","class":["col"],"props":{}}}}},"right":{"tag":"footer"}}};
-
 var Parser = require('./lib/Parser').parser
 	, Magic = new require('./lib/Magic').Magic(Parser);
 
